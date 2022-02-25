a888bee4c0e37b7ad3416c9c234dbd80e94cc0ee
Marcus Thomson <zizroc@nceas.ucsb.edu>
2022-02-25 09:53:05 -0800

    ignore scratchpad

--- R/scratchpad.R
+++ R/scratchpad.R
@@ -3,12 +3,21 @@ data_input <- reactive({
   fread(input$food_feed_path_csv$datapath)
 })
 
-loadData <- function() {
+drop_auth(rdstoken = "dropbox_token.rds")
+
+drop_auth(
+  new_user = FALSE,
+  key = "mmhfsybffdom42w",
+  secret = "l8zeqqqgm1ne5z0",
+  cache = TRUE
+)
+
+load_data_dropbox <- function() {
   # Read all the files into a list
-  filesInfo <- drop_dir("https://ucsb.box.com/s/ejb8tbhnzij8g0o7iw5ix7ie7dnopbx0")
+  filesInfo <- drop_dir("https://ucsb.box.com/s/5or22uc2xf97vop19c9fnfok5blq9fy0")
   filePaths <- filesInfo$path_display
-  data <- lapply(filePaths, drop_read_csv, stringsAsFactors = FALSE)
+  data_df <- lapply(filePaths, drop_read_csv, stringsAsFactors = FALSE)
   # Concatenate all data together into one data.frame
-  data <- do.call(rbind, data)
-  data
+  data_df <- do.call(rbind, data_df)
+  data_df
 }
